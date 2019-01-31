a53b2139f288d8690d93731ea312ca9b72ef1447
VaustXIII <vaustXIII@gmail.com>
2019-01-31 23:34:33 +0300

    Added a unity project
    Added project from gd50 intro to game development course to test out how well git handles unity

---
+++ assignment9/Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+
+    public float LowerThan = -1;
+    public GameObject[] ToDestroy;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < LowerThan)
+        {
+            SceneManager.LoadScene("GameOver");
+            foreach(var go in ToDestroy)
+            {
+                Destroy(go);
+            }
+        }
+	}
+}
---
+++ assignment9/Assets/Scripts/LevelTextController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTextController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var text = GetComponent<UnityEngine.UI.Text>();
+        text.text = "Level " + LevelGenerator.CurrentLevel;
+	}
+}
